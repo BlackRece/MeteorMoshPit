18e87e020205c08986a6efc65ddb2919238c8fc3
Maurice Thompson-Hamilton <mauricemlth@hotmail.com>
2023-08-11 08:35:02 +0100

    written test for the creation of a shape

--- CPP.MeterorMoshPit/Shape.h
+++ CPP.MeterorMoshPit/Shape.h
@@ -11,13 +11,21 @@ public:
 	~Shape();
 
 	void SetShape();
+
 	void SetFillColor(sf::Color color);
+	sf::Color GetFillColor() const { return m_fillColor; }
+
 	void SetLineColor(sf::Color color);
+	sf::Color GetLineColor() const { return m_lineColor; }
+
 	void SetLineThickness(float fThickness);
+	float GetLineThickness() const { return m_fLineThickness; }
+
 	void SetRadius(float fRadius);
+	float GetRadius() const { return m_fRadius; }
+
 	void SetPosition(sf::Vector2f v2fPosition);
 	sf::Vector2f GetPosition() const { return m_v2fPosition; }
-
 
 	// IDrawable
 	void Draw(sf::RenderWindow& window) override;
--- UnitTests/GivenAShape.cpp
+++ UnitTests/GivenAShape.cpp
@@ -1,5 +1,4 @@
 #include "pch.h"
-//#include "CppUnitTest.h"
 #include "../CPP.MeterorMoshPit/Shape.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
@@ -17,10 +16,36 @@ namespace GivenAShape
 			//Shape shape = Shape(v2fPosition, fRadius);
 		}
 
-		TEST_METHOD_CLEANUP(Teardown)
+		TEST_METHOD(ThenTheDefaultValuesAreAsExpected)
 		{
+			sf::Vector2f v2fExpectedPosition(0.f, 0.f);
+			float fExpectedRadius = 1.f;
+			sf::Color expectedFillColor = sf::Color::Black;
+			sf::Color expectedLineColor = sf::Color::White;
+			float fExpectedLineThickness = 1.f;
 
+			Shape shape = Shape(v2fExpectedPosition, fExpectedRadius);
+			sf::Vector2f v2fActualPosition = shape.GetPosition();
+			float fActualRadius = shape.GetRadius();
+			sf::Color actualFillColor = shape.GetFillColor();
+			sf::Color actualLineColor = shape.GetLineColor();
+			float fActualLineThickness = shape.GetLineThickness();
+
+			Assert::AreEqual(v2fExpectedPosition.x, v2fActualPosition.x);
+			Assert::AreEqual(v2fExpectedPosition.y, v2fActualPosition.y);
+			Assert::AreEqual(fExpectedRadius, fActualRadius);
+			
+			Assert::AreEqual(expectedFillColor.r, actualFillColor.r);
+			Assert::AreEqual(expectedFillColor.g, actualFillColor.g);
+			Assert::AreEqual(expectedFillColor.b, actualFillColor.b);
+
+			Assert::AreEqual(expectedLineColor.r, actualLineColor.r);
+			Assert::AreEqual(expectedLineColor.g, actualLineColor.g);
+			Assert::AreEqual(expectedLineColor.b, actualLineColor.b);
+
+			Assert::AreEqual(fExpectedLineThickness, fActualLineThickness);
 		}
+
 
 		TEST_METHOD(ThenThePositionIsAsExpected)
 		{
@@ -29,6 +54,10 @@ namespace GivenAShape
 
 			Shape shape = Shape(v2fExpectedPosition, fRadius);
 			sf::Vector2f v2fActualPosition = shape.GetPosition();
+			Assert::AreEqual(v2fExpectedPosition.x, v2fActualPosition.x);
+
+			shape.SetPosition(v2fExpectedPosition);
+			v2fActualPosition = shape.GetPosition();
 			Assert::AreEqual(v2fExpectedPosition.x, v2fActualPosition.x);
 		}
 
@@ -50,5 +79,11 @@ namespace GivenAShape
 			//sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
 			//shape.Draw(window);
 		}
+
+		TEST_METHOD_CLEANUP(Teardown)
+		{
+
+		}
+
 	};
 }
