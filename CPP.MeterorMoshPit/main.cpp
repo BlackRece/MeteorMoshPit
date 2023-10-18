c790cd00b114dc5ca586859bca5163c93b4f395b
Maurice Thompson-Hamilton <mauricemlth@hotmail.com>
2023-10-18 07:57:18 +0100

    removed unrequired includes from main.cpp

--- CPP.MeterorMoshPit/main.cpp
+++ CPP.MeterorMoshPit/main.cpp
@@ -1,13 +1,8 @@
-#include <SFML/Graphics.hpp>
-#include <string>
-#include "Ship.h"
-#include "GameWindow.h"
-#include "Asteroid.h"
 #include "Game.h"
 
 int main()
 {
-    //setup window
+    // setup window
     int iWidth = 600;
     int iHeight = 400;
     int iFPS = 60;
